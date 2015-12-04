b610b1f02877eda77ed744e3f4bc5dbaab970e3f
Fábio Silva <fabio.fernando.osilva@gmail.com>
2015-12-04 19:24:34 -0300

    updated qualcomm location services - prevents random fatal exceptions on AndroidRuntime   due to OutOfMemoryError thrown by com.qualcomm.location.LocationService
    Change-Id: I11689e3e43817dc60f74be4c4fa3bd883ad22fdb

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -92,6 +92,18 @@ LOCAL_MODULE_CLASS := JAVA_LIBRARIES
 LOCAL_CERTIFICATE := PRESIGNED
 include \$(BUILD_PREBUILT)
 
+include \$(CLEAR_VARS)
+LOCAL_MODULE := com.qualcomm.location.z3dual
+LOCAL_OVERRIDES_PACKAGES := com.qualcomm.location
+LOCAL_MODULE_OWNER := $VENDOR
+LOCAL_SRC_FILES := proprietary/priv-app/com.qualcomm.location/com.qualcomm.location.apk
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_SUFFIX := \$(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_MODULE_CLASS := APPS
+LOCAL_CERTIFICATE := platform
+LOCAL_PRIVILEGED_MODULE := true
+include \$(BUILD_PREBUILT)
+
 endif
 EOF
 fi
