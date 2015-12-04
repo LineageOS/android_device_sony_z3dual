29f14dc2df1837bda685cd3c1e7cbdef005a63f4
Fábio Silva <fabio.fernando.osilva@gmail.com>
2015-12-04 19:24:34 -0300

    updated qualcomm location services
    - prevents random fatal exceptions on AndroidRuntime
      due to OutOfMemoryError thrown by com.qualcomm.location.LocationService
    
    Change-Id: I11689e3e43817dc60f74be4c4fa3bd883ad22fdb

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -25,7 +25,8 @@ PRODUCT_PACKAGES += \\
     qcrilmsgtunnel \\
     qcnvitems \\
     qcrilhook \\
-    semcrilextension
+    semcrilextension \\
+    com.qualcomm.location-z3dual
 
 \$(call inherit-product, vendor/$VENDOR/$DEVICE/$DEVICE-vendor-blobs.mk)
 EOF
@@ -90,6 +91,20 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_SUFFIX := \$(COMMON_JAVA_PACKAGE_SUFFIX)
 LOCAL_MODULE_CLASS := JAVA_LIBRARIES
 LOCAL_CERTIFICATE := PRESIGNED
+include \$(BUILD_PREBUILT)
+
+include \$(CLEAR_VARS)
+LOCAL_MODULE := com.qualcomm.location-z3dual
+LOCAL_MODULE_PATH := \$(TARGET_OUT_APPS_PRIVILEGED)/com.qualcomm.location
+LOCAL_MODULE_FILENAME := com.qualcomm.location
+LOCAL_OVERRIDES_PACKAGES := com.qualcomm.location
+LOCAL_MODULE_OWNER := $VENDOR
+LOCAL_SRC_FILES := proprietary/priv-app/com.qualcomm.location/com.qualcomm.location.apk
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_SUFFIX := \$(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_MODULE_CLASS := APPS
+LOCAL_CERTIFICATE := platform
+LOCAL_PRIVILEGED_MODULE := true
 include \$(BUILD_PREBUILT)
 
 endif
