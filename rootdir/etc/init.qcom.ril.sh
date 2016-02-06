a63c9bcaf53b44deced4816e75f483939130513b
Fábio Silva <fabio.fernando.osilva@gmail.com>
2016-02-06 16:59:53 -0300

    z3dual: more proper ril-daemon initialization
    - also remove unnecessary service redefinition
    
    Change-Id: I406447bb48d87089c2515cb08ae874896df98ee3

--- rootdir/etc/init.qcom.ril.sh
+++ rootdir/etc/init.qcom.ril.sh
@@ -4,7 +4,5 @@ export PATH=/system/xbin:$PATH
 multisim=`getprop persist.radio.multisim.config`
 
 if [ "$multisim" = "dsds" ] || [ "$multisim" = "dsda" ]; then
-    stop ril-daemon
-    start ril-daemon
     start ril-daemon2
 fi
