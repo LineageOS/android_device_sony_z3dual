d9bdcddba4fb78c88085de2a0e9b18d95acb6a29
Steve Kondik <steve@cyngn.com>
2017-07-18 05:16:51 +0000

    z3dual: Use generic extract utils
    Change-Id: Iba6a91a15c4c93d5e5c0ec14047391e2a7e4fbf6

--- extract-files.sh
+++ extract-files.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
+#
+# Copyright (C) 2017 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
-# Use tradition sort
-export LC_ALL=C
+set -e
 
-FP=$(cd ${0%/*} && pwd -P)
-export VENDOR=$(basename $(dirname $FP))
-export DEVICE=$(basename $FP)
-export BOARDCONFIGVENDOR=true
+export DEVICE=z3dual
+export DEVICE_COMMON=shinano-common
+export PLATFORM_COMMON=msm8974-common
+export VENDOR=sony
 
-../common/extract-files.sh $@
-../common/setup-makefiles.sh
-
-./setup-makefiles.sh
+./../$PLATFORM_COMMON/extract-files.sh $@
