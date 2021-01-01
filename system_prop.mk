08023350b6b5850a8c71043bbef4cac2951ce209
Tom Roida <tom@roida.de>
2021-01-01 23:26:59 +0100

    z3dual: Remove redundant properties
    LCD and USB path are not specific for Z3 dual.
    Properties will be inherited from Z3.
    
    Change-Id: I8ee7528eaf17ba1bcadd24506ef2e81616833ebd

--- system_prop.mk
+++ system_prop.mk
@@ -42,11 +42,3 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Data modules
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.use_data_netmgrd=true
-
-# LCD
- PRODUCT_PROPERTY_OVERRIDES += \
-     ro.sf.lcd_density=480
-
-# USB path
- PRODUCT_PROPERTY_OVERRIDES += \
-     ro.usb.pid_suffix=1BA
